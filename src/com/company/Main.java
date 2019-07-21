e66a0e60c11ee22cf770f2403ff758ee8f81a643
frimanik <52167218+frimanik@users.noreply.github.com>
2019-07-21 19:03:15 +0500

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,6 +19,11 @@ public class Main {
         test.surfaceCircle();
     }
 
+    /*
+    Определить возраст с момента рождения  на момент запуска программы.
+    Возраст в секундах, минутах, часах, днях, месяцах и годах
+    */
+
     public void Age() {
         LocalDateTime start = LocalDateTime.now();
         LocalDateTime birthday = LocalDateTime.of(2000, 1, 21, 14, 00);
@@ -36,12 +41,23 @@ public class Main {
         System.out.println("Секунды:" + gap);
     }
 
+    /*
+    Даны две даты в виде 25.07.1921, т.е. день месяца 2 разряда,
+    месяц 2 разряда, год 4 разряда. Найти разницу в днях между
+    ними. Число должно быть всегда положительным
+    */
+
     public void dateDifference() {
         LocalDateTime firstDate = LocalDateTime.of(1923, 01, 01, 00, 00);
         LocalDateTime secondDate = LocalDateTime.of(1981, 12, 12, 00, 00);
         long gap = ChronoUnit.DAYS.between(firstDate, secondDate);
         System.out.println("Days between:" + abs(gap));
     }
+
+    /*
+    Дана строка вида Friday, Aug 10, 2016 12:10:56 PM.
+    Необходимо сконвертировать ее в вид “2018-0 8-10".
+    */
 
     public void formatDate() {
         String input = "Monday, Jul 15, 2019 12:00:00 PM";
@@ -50,6 +66,10 @@ public class Main {
         System.out.println(isoDateFormatter.format(LocalDate.parse(input, dateTimeFormatter)));
     }
 
+    /*
+    Необходимо посчитать площадь круга с указанным радиусом с точностью 50 знаков после запятой
+    */
+    
     public void surfaceCircle() {
         BigDecimal PI = new BigDecimal(3.14159265358979323846264338327950288419716939937510);
         BigDecimal radius = new BigDecimal(3);
